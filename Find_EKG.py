f75db21f6bc2c74b931b2de3a3d3be620a4ed38a
hwetsman <howard@tocdr.com>
2022-07-27 14:11:14 -0500

    Chore: erase old comments

--- Find_EKG.py
+++ Find_EKG.py
@@ -79,9 +79,7 @@ ekg_df = ekg_df[~ekg_df.clas.str.contains('Poor Recording')]
 year = st.sidebar.selectbox('Year of EKG', ['2019', '2020', '2021', '2022'])
 select_df = ekg_df[ekg_df.name.str.contains(year)]
 ekg_str = st.sidebar.selectbox('Select EKG', select_df.name.tolist(), index=0)
-# st.write(ekg_str)
 st.write(f'There are {ekg_df.shape[0]} EKGs after eliminating the {poor.shape[0]} poor recordings.')
-# a.write(ekg_df.head(1))
 
 # select and clean EKG to show
 ekg = Get_EKG(ekg_str)
@@ -89,7 +87,6 @@ this_classification = ekg_df.loc[ekg_df[ekg_df.name == ekg_str].index.tolist()[0
 st.write(f'You have selected {ekg_str}, classified as {this_classification}')
 ekg = Clean_EKG(ekg)
 
-# plot EKG
 
 # find the ekg peaks
 maxes = ekg.nlargest(200, 'peak')
@@ -98,43 +95,15 @@ peaks = ekg[ekg.peak > 0.5*max]
 # get single peaks
 singles = Get_Singles(peaks)
 
-# plot EKG and single peaks
+# plot EKG
 x = ekg.seconds
 y = ekg.micro_volts
 time0 = time.time()
 fig, ax = plt.subplots(figsize=(15, 4))
-# for peak in singles.seconds.tolist():
-#     plt.vlines(peak, -500, 1500, colors='r')
 ax.set_ylim(y.min(), y.max())
 plt.plot(x, y)
 st.pyplot(fig)
 time1 = time.time()
-# st.write(time1-time0)
 
-# Get PACs
-
-
-# def Get_PACs(singles):
-#     singles['interval'] = singles.seconds.shift(-1) - singles.seconds
-#     median = singles.interval.median()
-#     singles['med'] = median
-#     singles['sq_diff'] = (singles.med-singles.interval)*(singles.med-singles.interval)
-#     PACs = singles[singles.sq_diff > .01].shape[0]/2
-#     return PACs
-
-
-# add interval to singles
-# singles['interval'] = singles.seconds.shift(-1) - singles.seconds
-# median = singles.interval.median()
-# singles['med'] = median
-# singles['sq_diff'] = (singles.med-singles.interval)*(singles.med-singles.interval)
-# st.write(singles)
-# PACs = singles[singles.sq_diff > .01].shape[0]/2
 PACs = Get_PACs(singles)
 st.write(f'The EKG evidences {PACs} PACs')
-# st.write(round(.5, 1), round(.5, 0), round(.5, 2))
-
-# fig, ax = plt.subplots(figsize=(15, 4))
-# plt.hist(singles.sq_diff, bins=30)
-# plt.vlines(singles.sq_diff.median(), 0, 10, colors='r')
-# st.pyplot(fig)
